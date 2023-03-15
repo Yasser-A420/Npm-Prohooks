77bdc084d9813e34b86a115eec100577c4b187f5
root <yasserrose@icloud.com>
2023-03-15 13:44:54 +0000

    Use undici since people are so lazy to update node.js

--- src/Prohooks.ts
+++ src/Prohooks.ts
@@ -1,4 +1,5 @@
 import { ApiOptions, Data, BodyOptions, CreatedOrDeletedData } from "./index";
+import { request } from "undici";
 export class Prohooks {
     apiKey: string
     apiUrl: string;
@@ -8,48 +9,48 @@ export class Prohooks {
     }
     async create(options: BodyOptions): Promise<CreatedOrDeletedData> {
         return new Promise(async (resolve, reject) => {
-            const request = await fetch(`${this.apiUrl}/new`, { mode: "cors", headers: { authorization: this.apiKey, "Content-Type": "application/json" }, method: "PUT", body: JSON.stringify({ name: options.name, method: options.method, url: options.url, duration: options.duration, payload: options.payload, headers: options.headers, include_details: options.include_details ?? false }) });
-            if (request.status >= 200 && request.status < 300) {
-                const json = await request.json();
+            const response = await request(`${this.apiUrl}/new`, { headers: { authorization: this.apiKey, "Content-Type": "application/json" }, method: "PUT", body: JSON.stringify({ name: options.name, method: options.method, url: options.url, duration: options.duration, payload: options.payload, headers: options.headers, include_details: options.include_details ?? false }) });
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                const json = await response.body.json() as any;
                 resolve({message: json.message, scheduled: json.scheduled, error: json.error});
             } else {
-                const json = await request.text();
+                const json = await response.body.text();
                 reject(new Error(json));
             }
         });
     }
     async get(timerId: string): Promise<Data> {
         return new Promise(async (resolve, reject) => {
-            const request = await fetch(`${this.apiUrl}/timers/${timerId}`, { headers: { authorization: this.apiKey }, method: "GET" });
-            if (request.status >= 200 && request.status < 300) {
-                const json = await request.json() as Data;
+            const response = await request(`${this.apiUrl}/timers/${timerId}`, { headers: { authorization: this.apiKey }, method: "GET" });
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                const json = await response.body.json() as Data;
                 resolve(json);
             } else {
-                const json = await request.text();
+                const json = await response.body.text();
                 reject(new Error(json));
             }
         });
     }
     async getAll(): Promise<Data[]>{
         return new Promise(async (resolve, reject) => {
-            const request = await fetch(`${this.apiUrl}/timers/`, { headers: { authorization: this.apiKey }, method: "GET" });
-            if (request.status >= 200 && request.status < 300) {
-                const json = await request.json() as Data[];
+            const response = await request(`${this.apiUrl}/timers/`, { headers: { authorization: this.apiKey }, method: "GET" });
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                const json = await response.body.json() as Data[];
                 resolve(json);
             } else {
-                const json = await request.text();
+                const json = await response.body.text();
                 reject(new Error(json));
             }
         });
     }
     async delete(timerId: string): Promise<CreatedOrDeletedData> {
         return new Promise(async (resolve, reject) => {
-            const request = await fetch(`${this.apiUrl}/timers/${timerId}`, { headers: { authorization: this.apiKey }, method: "DELETE" });
-            if (request.status >= 200 && request.status < 300) {
-                const json = await request.json();
+            const response = await request(`${this.apiUrl}/timers/${timerId}`, { headers: { authorization: this.apiKey }, method: "DELETE" });
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                const json = await response.body.json() as any;
                 resolve({message: json.message, scheduled: "Cancelled", error: json.error});
             } else {
-                const json = await request.text();
+                const json = await response.body.text();
                 reject(new Error(json));
             }
         });
